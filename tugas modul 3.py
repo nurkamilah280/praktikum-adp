80aa272f78c35dbe3e6544bce59a1fcc0a4a00e3
nurkamilah280 <nurkamilahmila6@gmail.com>
2025-03-16 17:14:08 +0700

    Add files via upload

---
+++ tugas modul 3.py
@@ -0,0 +1,30 @@
+print('>>>>>>>>> SELAMAT DATANG <<<<<<<<<')
+pemain_1 = input('pemain 1 : ')
+pemain_2 = input('pemain 2 : ')
+print(pemain_1, 'Vs', pemain_2)
+print('pemain 1 :', pemain_1)
+n = int(input('masukkan batas angka positif : '))
+k = int(input('pilih angka BOM              : '))
+for i in range (1, n+1) :
+    if i % k == 0 :
+        print('BOM', end =' ')
+    else :
+        print(i, end =' ') 
+       
+print('pemain 2 :', pemain_2)
+m = int(input('tebak angka positif : '))
+while m > n :
+    print('angka yang anda masukkan melebihi batas rentang angka')
+    m = int(input('tebak ulang : '))
+faktor = 2 
+while faktor < k :
+    if k % faktor == 0 and m % faktor == 0 :
+        print( 'angka', m, 'adalah angka BOM', pemain_2,' anda kalah !')
+        break 
+    faktor += 1
+if m % k == 0 :
+    print( 'angka', m, 'adalah angka BOM', pemain_2,'anda kalah !')
+else : 
+    print('angka', m, 'bukan angka BOM, selamat', pemain_1,'anda menang!')
+
+            
